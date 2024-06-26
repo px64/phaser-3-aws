8bf3333e8d6a01d702431aea3efe8120d7bfc177
John Sievers <jsievers77@gmail.com>
2024-06-26 12:17:11 -0400

    Update dilemma.js

--- dilemma.js
+++ dilemma.js
@@ -33,6 +33,11 @@ var justiceWoke = 20;
 var justiceStrength = 5;
 var charVal = {};
 
+const ICON_MARGIN = 10;
+const GAUGE_HEIGHT = 50;
+const ICON_SPACING = 10;
+const ICON_SCALE = 0.03;
+
 //====================================================================================
 //
 //             findValidTerritory(thisFaction, otherFaction)
