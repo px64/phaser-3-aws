5b776ce28c5e345c79ab011ea316574ab5344606
John Sievers <jsievers77@gmail.com>
2024-07-11 11:58:27 -0400

    tweak rectangle some more

--- characterUtils.js
+++ characterUtils.js
@@ -269,13 +269,13 @@ export function introduceCharacters(scene, characters, sharedData) {
         helpsLabel.setDepth(3);
 
         // Add the "helps" icon slightly above the "helps" text
-        let helpsIcon = scene.add.image(helpsX, helpshurtsY-95, graphicObject);
+        let helpsIcon = scene.add.image(helpsX, helpshurtsY-130, graphicObject);
         helpsIcon.setScale(scaleFactor.helps);
         helpsIcon.setOrigin(0.5, 0.5);
         helpsIcon.setVisible(false);
         helpsIcon.setDepth(2);
         
-        let helpsBox = scene.add.rectangle(helpsIcon.x, helpshurtsY-95, helpsLabel.width + 40, 95 + 85 + 50, 0x000000, 1);
+        let helpsBox = scene.add.rectangle(helpsIcon.x, helpshurtsY-130, helpsLabel.width + 40, 130 + 85 + 150, 0x000000, 1);
         helpsBox.setStrokeStyle(2, character.faction === 'maga' ? 0xff4040 : 0x8080ff, 0.8);
         helpsBox.isStroked = true;
         helpsBox.setOrigin(0.5, 0.5);
